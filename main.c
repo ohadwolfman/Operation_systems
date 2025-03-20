1fcf981edab3da39c0cefbe1072f57f5917cc647
Ohad Wolfman <98156296+ohadwolfman@users.noreply.github.com>
2025-03-20 23:09:37 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "Task1.c"
+
+void CreateOutputFile();
+//void PitagoricalThree(int a, int b, int c);
+void PitagoricalThree(int argc, char *argv[]);
+
+int main(int argc, char *argv[]) {
+    CreateOutputFile();
+    PitagoricalThree(argc, argv);
+    PitagoricalThree(argc, argv);
+    printf("Task1 completed\n");
+    return 0;
+}
