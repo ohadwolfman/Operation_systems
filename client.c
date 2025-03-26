e988976ebbfeb10cd7dadc1cb5f93cdaebb5275f
Ohad Wolfman <98156296+ohadwolfman@users.noreply.github.com>
2025-03-26 22:38:30 +0200

    Update client.c

--- client.c
+++ client.c
@@ -34,36 +34,49 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    for (int i = 0; i < 6; i++) {
-        unsigned char side = (unsigned char)(rand() % 15);
-        if (send(client_socket, &side, sizeof(side), 0) < 0) {
-            perror("Error sending data");
-            close(client_socket);
-            return 1;
+    // Send 3 random numbers and receive the response
+    for (int i = 0; i < 2; i++) { //loop twice, once for random numbers, once for good numbers
+        unsigned char sides[3];
+        if (i == 0) {
+            for (int j = 0; j < 3; j++) {
+                sides[j] = (unsigned char)(rand() % 15);
+            }
+        } else {
+            sides[0] = 5;
+            sides[1] = 12;
+            sides[2] = 13;
         }
-        sleep(1);
-        printf("Sent: %d\n", side);
-//        fflush(stdout);
+
+        for (int j = 0; j < 3; j++) {
+            if (send(client_socket, &sides[j], sizeof(sides[j]), 0) < 0) {
+                perror("Error sending data");
+                close(client_socket);
+                return 1;
+            }
+            printf("Sent: %d\n", sides[j]);
+            fflush(stdout);
+        }
+
+        // Receive the server's response
+        char response[256];
+        int bytes_received = recv(client_socket, response, sizeof(response) - 1, 0);
+        if (bytes_received > 0) {
+            response[bytes_received] = '\0';
+            printf("Server response: %s", response);
+            fflush(stdout);
+        } else if (bytes_received == 0) {
+            printf("Server closed the connection.\n");
+            fflush(stdout);
+            break; // Exit the loop if the server closes the connection
+        } else {
+            perror("Error receiving data");
+            break; // Exit the loop on error
+        }
     }
 
     // Shut down sending side
     if (shutdown(client_socket, SHUT_WR) < 0) {
         perror("Error shutting down write direction");
-    }
-
-    char response[256];
-    int bytes_received;
-    while ((bytes_received = recv(client_socket, response, sizeof(response) - 1, 0)) > 0) {
-        response[bytes_received] = '\0';
-        printf("Server response: %s", response);
-        fflush(stdout);
-    }
-
-    if (bytes_received == 0) {
-        printf("Server closed the connection.\n");
-        fflush(stdout);
-    } else if (bytes_received < 0) {
-        perror("Error receiving data");
     }
 
     close(client_socket);
